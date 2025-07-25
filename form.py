f5d5bf26ce3d6dd123c760e70b38ee8deda2afe8
GuybrushMcfly <113950867+GuybrushMcfly@users.noreply.github.com>
2025-07-25 00:03:06 -0300

    Update form.py

--- form.py
+++ form.py
@@ -155,9 +155,9 @@ if selected and selected[0].get("Comisión") != "Sin comisiones":
     st.markdown(
         f"""
         <h4>2. Validá tu CUIL para inscribirte en</h4>
-        <span style="color:#b72877;font-weight:bold; font-size:1.15em;">
+        <div style="color:#b72877;font-weight:bold; font-size:1.25em; margin-top:-0.7em;">
             {actividad_nombre} ({comision_nombre})
-        </span>
+        </div>
         """,
         unsafe_allow_html=True
     )
