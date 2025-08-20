b934f02ef9a8c495bb8da7a184800d71e27dffbe
GuybrushMcfly <113950867+GuybrushMcfly@users.noreply.github.com>
2025-08-20 00:30:57 -0300

    Update form.py

--- form.py
+++ form.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from datetime import date, datetime
-from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
 from supabase import create_client, Client
 from collections import defaultdict
 import time
