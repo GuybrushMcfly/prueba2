07e0a1e6510731b78cf3b8b1cf0bf49ce0150aae
GuybrushMcfly <113950867+GuybrushMcfly@users.noreply.github.com>
2025-08-21 19:33:48 -0300

    Update form.py

--- form.py
+++ form.py
@@ -550,8 +550,8 @@ st.markdown("""
 <style>
 .flip-card {
   background-color: transparent;
-  width: 380px;
-  height: 200px;
+  width: 430px;
+  height: 230px;
   perspective: 1000px;
   margin: 10px auto;
 }
